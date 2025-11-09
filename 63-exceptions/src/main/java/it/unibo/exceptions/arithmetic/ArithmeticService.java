725079d51d94f3230f164c8c2dbcc8e9ab1f7786
Lorenzo Baldazzi <lorenzo.baldazzi2@studio.unibo.it>
2025-11-09 08:51:53 +0100

    Use finally to compute even after the

--- 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
@@ -56,7 +56,8 @@ public final class ArithmeticService {
      * @return the result of the process
      */
     public String process() {
-        if (commandQueue.isEmpty()) {
+        try {
+            if (commandQueue.isEmpty()) {
             throw new IllegalStateException("No commands sent, no result available");
         }
         while (commandQueue.size() != 1) {
@@ -93,6 +94,10 @@ public final class ArithmeticService {
          * The commandQueue should be cleared, no matter what, when the method exits
          * But how?
          */
+        }finally{
+            commandQueue.clear();
+        }
+        
     }
 
     private void computeAt(final int operatorIndex) {
