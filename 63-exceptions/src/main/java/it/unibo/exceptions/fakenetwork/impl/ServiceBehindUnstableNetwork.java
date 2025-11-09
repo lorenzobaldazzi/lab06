a384ff92c3d4b538c64bb7307a686e8c16e0ca5a
Lorenzo Baldazzi <lorenzo.baldazzi2@studio.unibo.it>
2025-11-09 07:19:03 +0100

    completamento punto 1: controllo della probabilità [0,1[

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,9 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if(failProbability < 0 || failProbability >= 1){
+            throw new IllegalArgumentException("La probabiltà deve essere [0,1[ : " + failProbability);
+        }
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
